02d9d3fb96cdc671834d9202712109284fba783c
Utsav Shukla <us241077@gmail.com>
2017-11-26 20:19:51 +0530

    First commit

---
+++ astros.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import json
+import turtle
+import urllib.request
+url='http://api.open-notify.org/astros.json'
+response=urllib.request.urlopen(url)
+result=json.loads(response.read())
+print('People in Space:',result["number"])
+people=result['people']
+for p in people:
+  print(p['name'],'in', p['craft'])
+
+  
